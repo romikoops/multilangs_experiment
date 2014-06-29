fd74141da0f0d64c0719e15a8ed2eb3c4ae63029
Roman Parashchenko <romikoops1@gmail.com>
2014-06-29 20:54:30 +0200

    Created blank web app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultilangsExperiment::Application.config.secret_token = '2883672b7f7d3ed34633bfecf3631302de107877fe233b62fee17cd83c4083fda4c18b91d90585d1629fe1da881285441e793490f3ac1978d8cc483d13bad1a0'
