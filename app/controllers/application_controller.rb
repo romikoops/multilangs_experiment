bfc6261f1f5816bd3b8c03c0bc16d73006be9d15
Roman Parashchenko <romikoops1@gmail.com>
2014-06-29 22:48:06 +0200

    Added test model and standard I18n multi language functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+    Rails.application.routes.default_url_options[:locale] = I18n.locale
+  end
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 end
